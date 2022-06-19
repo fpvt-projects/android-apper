313e23bd8816aff6884c2703f39f1e2d7860b185
Francis Paolo <francispaolo.talan@gmail.com>
2022-06-19 21:47:52 +0800

    kotlin-changes-00

--- GCASH/app/src/main/java/ph/gcash/caddet/francis/talan/sample/MainActivity.kt
+++ GCASH/app/src/main/java/ph/gcash/caddet/francis/talan/sample/MainActivity.kt
@@ -2,10 +2,17 @@ package ph.gcash.caddet.francis.talan.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.gcash.caddet.francis.talan.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textMessage.text = "View Binding"
     }
 }
