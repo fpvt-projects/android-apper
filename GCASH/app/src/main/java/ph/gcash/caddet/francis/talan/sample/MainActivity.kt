644f289135b7403e2b5c89d62705f58ee40ebe0f
Francis Paolo <francispaolo.talan@gmail.com>
2022-06-19 21:28:56 +0800

    first commit

---
+++ GCASH/app/src/main/java/ph/gcash/caddet/francis/talan/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.gcash.caddet.francis.talan.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
